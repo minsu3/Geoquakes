626cfb408027e69c58a34ea2f45a83bb1d73354c
justincastilla <pxlperfection@mail.com>
2018-06-25 15:17:29 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React, { Component } from 'react';
-import Map from './components/Map'
-import Quakes from './components/Quakes'
-import QuakeModel from './models/Quake'
 
 class App extends Component {
 
-  constructor(){
-    super()
-
-    this.state = {
-      quakes : []
-    }
-    
-  }
-
-  componentDidMount(){ 
-    QuakeModel.all().then(res => 
-      this.setState({
-        quakes: res.data.features
-      }) 
-    )
-  }
 
   render() {
     return (
       <div className="app">
         <div className="mapContainer">
-          <Map quakes={this.state.quakes} />
+          ...put Map Component here...
         </div>
         <div className="quakeContainer">
           <h1>Earthquakes from the past week: </h1>
-          <Quakes quakes={this.state.quakes}/>
+          ...put Quakes Component here...
         </div>
       </div>
     );
