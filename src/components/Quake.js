29fe9e3f2649f0b6fdb268e1868f408576bf52b6
justincastilla <pxlperfection@mail.com>
2018-06-25 14:56:57 -0700

    added solution branch with quake sizes

--- src/components/Quake.js
+++ src/components/Quake.js
@@ -6,11 +6,11 @@ class Quake extends Component {
   render(){
     let quakeStyle = {
       fontSize: parseFloat(this.props.mag) * 10 - 33,
-      color: this.props.color,
+ 
     }
     return (
       <li>
-        <span className={this.props.shake ? 'shakey': null} style={quakeStyle}>
+        <span style={quakeStyle}>
          {this.props.title}
         </span>
       </li>
