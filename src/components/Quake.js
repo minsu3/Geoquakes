9c7ec175704f14a02d0ff8f8ba16396e60cc0303
justincastilla <pxlperfection@mail.com>
2018-06-25 14:57:52 -0700

    added solution branch

--- src/components/Quake.js
+++ src/components/Quake.js
@@ -4,15 +4,10 @@ class Quake extends Component {
 
 
   render(){
-    let quakeStyle = {
-      fontSize: parseFloat(this.props.mag) * 10 - 33,
- 
-    }
+
     return (
       <li>
-        <span style={quakeStyle}>
          {this.props.title}
-        </span>
       </li>
     )
     
